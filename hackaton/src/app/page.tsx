760c0490bc62e68d82166a8ef62ca215eb127166
JulianaFFF <lj.ferreira@uniandes.edu.co>
2025-10-08 09:04:16 -0500

    Retos 3 y 7 en la main page

--- hackaton/src/app/page.tsx
+++ hackaton/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import BarPage from "./punto1/reto1";
 import ClickCounter from "./punto2/reto2";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -12,8 +13,20 @@ export default function Home() {
         <h1 className="text-4xl font-bold text-center sm:text-left"> Punto 2 </h1>
         <ClickCounter />
         <h1 className="text-4xl font-bold text-center sm:text-left"> Punto 3 </h1>
+        <Link
+            href="/punto3"
+            className="rounded-full border border-solid border text-white transition-colors flex items-center justify-center gap-2 hover:bg-blue-600 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+          >
+            Jugar Tic-Tac
+          </Link>
+        <h1 className="text-4xl font-bold text-center sm:text-left"> Punto 7 </h1>
+          <Link
+            href="/punto7"
+            className="rounded-full border border-solid border text-white transition-colors flex items-center justify-center gap-2 hover:bg-blue-600 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+          >
+            Frases motivacionales
+          </Link>
 
-        
       </main>
     </div>
   );
